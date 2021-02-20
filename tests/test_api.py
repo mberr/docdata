2212aec6cd798715622b9e7b8422d8006818caaf
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-02-20 19:02:45 +0100

    Update tests and include non flake8 compliant examples

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,6 +7,21 @@ import unittest
 from docdata import docdata, parse_docdata
 
 
+class B:
+    """This class has a docdata."""
+
+
+class D:
+    """This class has a docdata.
+
+    :param args: Nope.
+    """
+
+    def __init__(self, *args):
+        """Initialize the class with dummy args."""
+        self.args = args
+
+
 class TestParse(unittest.TestCase):
     """Test parsing docdata."""
 
@@ -15,8 +30,20 @@ class TestParse(unittest.TestCase):
         self.assertIsNone(docdata(b))
         self.assertEqual({'name': 'A'}, docdata(a))
 
-    def test_parse_no_params(self):
-        """Test parsing docdata."""
+    def test_parse_no_params_no_newline(self):
+        """Test parsing docdata with no params, and no trailing space.."""
+
+        @parse_docdata
+        class A:
+            """This class has a docdata.
+            ---
+            name: A
+            """  # noqa: D205
+
+        self._help(A, B)
+
+    def test_parse_no_params_one_newline(self):
+        """Test parsing docdata with no params, and a newline before the delimiter."""
 
         @parse_docdata
         class A:
@@ -26,16 +53,28 @@ class TestParse(unittest.TestCase):
             name: A
             """
 
-        class B:
-            """This class has a docdata."""
-
         self._help(A, B)
 
-    def test_parse_with_params(self):
-        """Test parsing docdata."""
+    def test_parse_no_params_many_newline(self):
+        """Test parsing docdata with no params, and a newline before the delimiter."""
 
         @parse_docdata
         class A:
+            """This class has a docdata.
+
+
+
+            ---
+            name: A
+            """  # noqa: D205
+
+        self._help(A, B)
+
+    def test_parse_with_params_no_newline(self):
+        """Test parsing docdata."""
+
+        @parse_docdata
+        class C:
             """This class has a docdata.
 
             :param args: Nope.
@@ -44,15 +83,47 @@ class TestParse(unittest.TestCase):
             """
 
             def __init__(self, *args):
+                """Initialize the class with dummy args."""
                 self.args = args
 
-        class B:
+        self._help(C, D)
+
+    def test_parse_with_params_one_newline(self):
+        """Test parsing docdata."""
+
+        @parse_docdata
+        class C:
             """This class has a docdata.
 
             :param args: Nope.
+
+            ---
+            name: A
             """
 
             def __init__(self, *args):
+                """Initialize the class with dummy args."""
                 self.args = args
 
-        self._help(A, B)
+        self._help(C, D)
+
+    def test_parse_with_params_many_newline(self):
+        """Test parsing docdata."""
+
+        @parse_docdata
+        class C:
+            """This class has a docdata.
+
+            :param args: Nope.
+
+
+
+            ---
+            name: A
+            """
+
+            def __init__(self, *args):
+                """Initialize the class with dummy args."""
+                self.args = args
+
+        self._help(C, D)
